26d7dcce98f8d51a673e854c6f60aac48c7decdd
MSVenkat1010 <kmanjusai.venkat@gmail.com>
2024-02-18 12:44:52 +0530

    #1802

--- GuessTheNumber/script.js
+++ GuessTheNumber/script.js
@@ -1,41 +1,86 @@
-'use strict';
+"use strict";
 //Math.trunc(Math.random()*20)+1
-const secretNumber = 50
-
-function objectMatch(emoji="?",desc="Start guessing..."){
-    return {emotion:emoji,description:desc}
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let currentScore = 20;
+let highestscore = 0;
+let range = {};
+function objectMatch(
+  emoji = "?",
+  desc = "Start guessing...",
+  score = currentScore,
+  won = false
+) {
+  if (won) {
+    if (highestscore < score) {
+      highestscore = score;
+    }
+  }
+  return {
+    emotion: emoji,
+    description: desc,
+    score: score,
+    hS: highestscore,
+    won: won,
+  };
 }
 
+function reset(flag) {
+  currentScore = 20;
+  document.querySelector(".guess").value = "";
+  if (flag == "won") {
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".highscore").textContent = range.hS;
+  }
+  else if (flag == "again") {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".score").textContent = currentScore;
+    document.querySelector(".emoji").textContent = "?";
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector("body").style.backgroundColor = "#222";
+  }
+}
 //document.querySelector(".message").textContent = "Correct number!";
-      
-document.querySelector(".check").addEventListener('click', ()=>{
-   let guess = Number(document.querySelector(".guess").value)
-   let range={};
-   if(!guess){
-    console.log("⛔️ No Number!")
-    range=objectMatch()
-   }else{
-    let guessRange = Math.abs(secretNumber - guess)
-    range = guessRange === 0 ? objectMatch("😀","Found it")
-    : guessRange <5 ? objectMatch("🔥","Hotter")
-    : guessRange <10 ? objectMatch("🥵","Hot")
-    : guessRange <20 ? objectMatch("😶‍🌫️","warmer")
-    : guessRange <30? objectMatch("🤧","cold")
-    : guessRange <40 ? objectMatch("🥶","colder")
-    :objectMatch("☃️","coldest")
-    console.log("Guess=>",guess,secretNumber,guessRange)
-       }
-       console.log(range)
-    document.querySelector(".emoji").textContent = range.emotion
-    document.querySelector(".message").textContent = range.description
 
+document.querySelector(".check").addEventListener("click", () => {
+  let guess = Number(document.querySelector(".guess").value);
 
-})
-/**
- 
+  if (!guess) {
+    console.log("⛔️ No Number!");
+    range = objectMatch();
+  } else {
+    let guessRange = Math.abs(secretNumber - guess);
+    range =
+      guessRange === 0
+        ? objectMatch(
+            "😀",
+            "🎉 Congratulations! You guessed the correct number! 🎉",
+            currentScore,
+            true
+          )
+        : guessRange < 5
+        ? objectMatch(
+            "🔥",
+            "You're very close! Your guess is very hot!",
+            --currentScore
+          )
+        : guessRange < 10
+        ? objectMatch("🌞", "You're close! Your guess is hot!", --currentScore)
+        : guessRange < 20
+        ? objectMatch("😶‍🌫️", "You're getting warmer!", --currentScore)
+        : guessRange < 30
+        ? objectMatch("🌨️", "You're cold!", --currentScore)
+        : guessRange < 40
+        ? objectMatch("🥶", "You're very cold", --currentScore)
+        : objectMatch("☃️", "You're freezing!", --currentScore);
+  }
+  document.querySelector(".emoji").textContent = range.emotion;
+  document.querySelector(".message").textContent = range.description;
+  document.querySelector(".score").textContent = range.score;
+  if (range.won) {
+    reset("won");
+  }
+});
 
-             50 Hotter 55 Hot 60 warmer 70 cold 80 colder 90> coldest
-                  5          10       20      30      40          50
-               
-
- */
+document.querySelector(".again").addEventListener("click", function () {
+  reset("again");
+});
